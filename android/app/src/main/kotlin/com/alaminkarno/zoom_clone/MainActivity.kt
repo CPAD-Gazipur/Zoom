f71d5f8ac9f4960acf50b7b28052d1263bddb963
alamin-karno <amin15-1951@diu.edu.bd>
2022-10-06 11:38:17 +0600

    Zoom: Initialize Zoom App with Login Screen and Colors.

---
+++ android/app/src/main/kotlin/com/alaminkarno/zoom_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alaminkarno.zoom_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/screens.dart';
+import 'package:zoom_clone/utils/utils.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zoom: Meeting App',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+      },
+      home: const LoginScreen(),
+    );
+  }
+}
